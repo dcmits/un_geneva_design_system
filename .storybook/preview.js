863e18b56965c43eb97be41b365c47a5c13c2354
Bogdan Dinu <dinu.bogdan.radu@gmail.com>
2023-10-19 12:42:56 +0300

    Remove fontawesome from bundled css, replace home icon with svg

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,7 @@ import { setupTwig } from './setupTwig';
 
 // GLOBAL CSS
 import '../components/style.scss';
+import '../components/fontawesome.scss';
 
 // If in a Drupal project, it's recommended to import a symlinked version of drupal.js.
 import './_drupal.js';
