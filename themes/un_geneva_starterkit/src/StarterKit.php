53500247d406235bc0eef22304741b7221e72a42
Bogdan Dinu <dinu.bogdan.radu@gmail.com>
2023-10-23 12:35:53 +0300

    Update footer regions

--- themes/un_geneva_starterkit/src/StarterKit.php
+++ themes/un_geneva_starterkit/src/StarterKit.php
@@ -39,6 +39,15 @@ final class StarterKit implements StarterKitInterface {
         rename($working_dir . '/config/install/' . $basename, $working_dir . '/config/install/' . str_replace('un_geneva_starterkit', $machine_name, $basename));
       }
     }
+    if(glob("$working_dir/config/optional/*.yml")) {
+      foreach(glob("$working_dir/config/optional/*.yml") as $file) {
+        $content = file_get_contents($file);
+        $content = str_replace('un_geneva_starterkit', $machine_name, $content);
+        file_put_contents($file, $content);
+        $basename = basename($file);
+        rename($working_dir . '/config/optional/' . $basename, $working_dir . '/config/optional/' . str_replace('un_geneva_starterkit', $machine_name, $basename));
+      }
+    }
 
     // remove starterkit postprocess class
 
