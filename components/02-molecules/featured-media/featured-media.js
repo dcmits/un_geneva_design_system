22ebbe817075506090e36659aa5912df0c2ee0b5
Bogdan Dinu <dinu.bogdan.radu@gmail.com>
2024-02-19 15:57:27 +0200

    Featured media resize on mobile

--- components/02-molecules/featured-media/featured-media.js
+++ components/02-molecules/featured-media/featured-media.js
@@ -1,5 +1,16 @@
 Drupal.behaviors.featuredMedia = {
   attach(context) {
-    jQuery('.colorbox-iframe').colorbox({iframe: true, width: '90%', height: '90%', title: false});
+    jQuery('.colorbox-iframe').colorbox({
+      iframe: true,
+      width: '90%',
+      height: '90%',
+      maxHeight: '100%',
+      title: false,
+      onComplete: function() {
+        jQuery(this).colorbox.resize({
+          height: Math.min(jQuery(window).height()*0.9,jQuery("#colorbox").width()/1.46)
+        });
+      }
+    });
   }
 };
